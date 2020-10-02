e0021fe655631469609cba7685f176be1922275a
sbamihan <winsher.amihan@gmail.com>
2020-10-02 17:01:33 +0800

    updated

--- eBill/ebill-api/src/test/java/com/aboitiz/ebillapi/EbillApiApplicationTests.java
+++ eBill/ebill-api/src/test/java/com/aboitiz/ebillapi/EbillApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.aboitiz.billstager;
+package com.aboitiz.ebillapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BillStagerApplicationTests {
+class EbillApiApplicationTests {
 
 	@Test
 	void contextLoads() {
