4556b062787eaa6590d7d2e469e772e91da53c69
sbamihan <winsher.amihan@gmail.com>
2020-10-08 15:23:56 +0800

    updated

--- eBill/subscription-service/src/main/resources/data-types.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO subscription_type (type_code, description) VALUES ('EBIL', 'Electronic sending of bill');
-INSERT INTO subscription_type (type_code, description) VALUES ('OUTN', 'Outage notification');
-
-INSERT INTO contact_type (type_code, description) VALUES ('EADD', 'Email Address');
-INSERT INTO contact_type (type_code, description) VALUES ('SMSN', 'SMS Number');
-INSERT INTO contact_type (type_code, description) VALUES ('PHON', 'Phone Number');
-
-COMMIT;
