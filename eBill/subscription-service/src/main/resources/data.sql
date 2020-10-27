cf8827b982b1d066813d309d3294818118e29912
sbamihan <sherwin.amihan@aboitiz.com>
2020-10-27 09:53:24 +0800

    updated

--- eBill/subscription-service/src/main/resources/data.sql
+++ eBill/subscription-service/src/main/resources/data.sql
@@ -1,5 +1,6 @@
 INSERT INTO customer.subscription_type (type_code, description) VALUES ('EBIL', 'Electronic sending of bill');
 INSERT INTO customer.subscription_type (type_code, description) VALUES ('OUTN', 'Outage notification');
+INSERT INTO customer.subscription_type (type_code, description) VALUES ('MOAP', 'MobileAP application');
 
 INSERT INTO customer.contact_type (type_code, description) VALUES ('EADD', 'Email Address');
 INSERT INTO customer.contact_type (type_code, description) VALUES ('SMSN', 'SMS Number');
